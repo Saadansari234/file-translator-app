ec411ab8e8776d5ceacae2bdea52643e858f07c8
saadansari234 <saadansari3212@gmail.com>
2025-03-05 17:02:32 +0530

    hh

--- application/xliff_file_app/views.py
+++ application/xliff_file_app/views.py
@@ -6,34 +6,38 @@ from django.core.files.storage import default_storage
 from .utils.script4 import translate_file, save_xliff_file
 import os
 from django.http import FileResponse, HttpResponseNotFound
+
 # Create your views here.
 
 
 
 
-def index(request):
-    return render(request, "index.html")
+# def index(request):
+#     return render(request, "index.html")
 
 
 
 source_data = []
 translated_data = []
 
-def translate_xliff_view(request):
-
+def index(request):
+    global source_data, translated_data
     if request.method == "POST" and request.FILES.get("xliff_file"):
         xliff_file = request.FILES["xliff_file"]
 
-        # Save the uploaded file temporarily
+       
         file_path = default_storage.save(f"temp/{xliff_file.name}", xliff_file)
+
         print(file_path)
         request.session["file_path"] = file_path
-        source_data, translated_data = translate_file(file_path)
-        context = {
-             "paired_data": list(zip(source_data, translated_data))  # Pair source & translated text
-             }
-        return render(request, "index.html", context)
 
+        source_data, translated_data = translate_file(file_path)
+            # Store translated data in session
+
+        context = {
+                "paired_data": list(zip(source_data, translated_data))  # Pair source & translated text
+            }
+        return render(request, "index.html", context)
     return render(request, "index.html")
 
 
@@ -56,11 +60,13 @@ def update_translations(request):
         print("File path:", file_path)
 
         save_xliff_file(file_path, translated_data)  
+        os.remove(file_path)
 
         return JsonResponse({"message": "Success", "status": "ok"})
         
     except json.JSONDecodeError:
         return JsonResponse({"message": "Invalid JSON data", "status": "error"}, status=400)
+    
 
 
 def download_edited_File(request):
@@ -77,6 +83,33 @@ def download_edited_File(request):
              print("responssssssssssssssssseeeeeeeeeeee", response)
              return response
       else:   
-              return HttpResponseNotFound("File not found.")       
+              return HttpResponseNotFound("File not found.")
+      
+
+             
+def download_File(request):
+   if request.method == "GET":
+      file_path = request.session.get("file_path", "") 
+      print("sadddddddddddddddddddddddddddddddddddddd", file_path)
+      print("sadddddddddddddddddddddddddddddddddddddd", translated_data)
+      save_xliff_file(file_path, translated_data)  
+      os.remove(file_path)
+
+      translated_file_path = "media/translated.xlf"
+      
+
+      if os.path.exists(translated_file_path):  
+             response = FileResponse(
+             open(translated_file_path, "rb"), 
+             as_attachment=True, 
+             filename="translated.xlf"
+             )
+             response['Content-Type'] = 'application/x-xliff+xml'  # Ensure correct MIME type
+             print("responssssssssssssssssseeeeeeeeeeee", response)
+             return response
+      else:   
+              return HttpResponseNotFound("File not found.")    
+
+        
 
 
